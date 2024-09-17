e58c9339c2dcba90b515cde61c7068093265925d
JuliaK18 <juliakbgamer12@gmail.com>
2024-09-17 13:11:04 +0200

    first commit

---
+++ buscador-tallas-jk.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Buscador de Tallas
+Description: Un plugin para crear un buscador de tallas de neumáticos.
+Version: 1.0
+Author: Júlia Krukonis
+*/
+
+// Shortcode para mostrar el formulario
+
+
+// Cargar el JavaScript
